1b61591838b382da029782d9d408eb2f79e5384a
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2024-12-26 20:12:06 +0100

    Extract parser

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 // see https://github.com/shanteacontrols/OpenDeck/wiki/Sysex-Configuration
 
+mod parser;
+
 const SYSEX_START: u8 = 0xF0;
 const SYSEX_END: u8 = 0xF7;
 const M_ID_0: u8 = 0x00;
@@ -52,20 +54,10 @@ pub enum MessageStatus {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum OpenDeckMsg {
+pub enum OpenDeckRequest {
     Special(SpecialRequest),
     Configuration,
     ComponentInfo,
-    Status,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum OpenDeckParseError {
-    BufferTooShort,
-    NoSysex,
-    WrongManufacturer,
-    StatusError(MessageStatus),
 }
 
 enum ByteOrder {
@@ -88,43 +80,6 @@ impl ByteOrder {
     }
 }
 
-pub struct OpenDeckParser {}
-
-impl OpenDeckParser {
-    pub fn parse(buf: &[u8]) -> Result<OpenDeckMsg, OpenDeckParseError> {
-        if buf.len() < 8 {
-            return Err(OpenDeckParseError::BufferTooShort);
-        }
-        if ByteOrder::Start.get(buf) != SYSEX_START {
-            return Err(OpenDeckParseError::NoSysex);
-        }
-        if buf[buf.len() - 1] != SYSEX_END {
-            return Err(OpenDeckParseError::NoSysex);
-        }
-
-        if !(ByteOrder::Id1.get(buf) == M_ID_0
-            && ByteOrder::Id2.get(buf) == M_ID_1
-            && ByteOrder::Id3.get(buf) == M_ID_2)
-        {
-            return Err(OpenDeckParseError::WrongManufacturer);
-        }
-
-        if !ByteOrder::Status.get(buf) == MessageStatus::Request as u8 {
-            return Err(OpenDeckParseError::StatusError(MessageStatus::StatusError));
-        }
-        if buf.len() == SPECIAL_REQ_MSG_SIZE {
-            return OpenDeckParser::parse_special_request(buf);
-        }
-
-        Err(OpenDeckParseError::BufferTooShort)
-    }
-
-    pub fn parse_special_request(buf: &[u8]) -> Result<OpenDeckMsg, OpenDeckParseError> {
-        let special = SpecialRequest::try_from(ByteOrder::Wish.get(buf))?;
-        Ok(OpenDeckMsg::Special(special))
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum SpecialRequest {
@@ -142,100 +97,23 @@ pub enum SpecialRequest {
     BootloaderSupport = 0x51,
     Backup = 0x1B,
 }
-
-impl TryFrom<u8> for SpecialRequest {
-    type Error = OpenDeckParseError;
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            x if x == SpecialRequest::Handshake as u8 => Ok(SpecialRequest::Handshake),
-            x if x == SpecialRequest::ValueSize as u8 => Ok(SpecialRequest::ValueSize),
-            x if x == SpecialRequest::ValuesPerMessage as u8 => {
-                Ok(SpecialRequest::ValuesPerMessage)
-            }
-            x if x == SpecialRequest::FirmwareVersion as u8 => Ok(SpecialRequest::FirmwareVersion),
-            x if x == SpecialRequest::FirmwareVersionAndHardwareUUID as u8 => {
-                Ok(SpecialRequest::FirmwareVersionAndHardwareUUID)
-            }
-            x if x == SpecialRequest::HardwareUUID as u8 => Ok(SpecialRequest::HardwareUUID),
-            x if x == SpecialRequest::NrOfSupportedComponents as u8 => {
-                Ok(SpecialRequest::NrOfSupportedComponents)
-            }
-            x if x == SpecialRequest::Reboot as u8 => Ok(SpecialRequest::Reboot),
-            x if x == SpecialRequest::BootloaderMode as u8 => Ok(SpecialRequest::BootloaderMode),
-            x if x == SpecialRequest::FactoryReset as u8 => Ok(SpecialRequest::FactoryReset),
-            x if x == SpecialRequest::NrOfSupportedPresets as u8 => {
-                Ok(SpecialRequest::NrOfSupportedPresets)
-            }
-            x if x == SpecialRequest::Backup as u8 => Ok(SpecialRequest::Backup),
-            x if x == SpecialRequest::BootloaderSupport as u8 => {
-                Ok(SpecialRequest::BootloaderSupport)
-            }
-            _ => Err(OpenDeckParseError::StatusError(MessageStatus::StatusError)),
-        }
-    }
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum ValueSize {
+    OneByte = 1,
+    TwoBytes = 2,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_parse_special_messages() {
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x01, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::Handshake))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x02, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::ValueSize))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x03, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::ValuesPerMessage))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x56, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::FirmwareVersion))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x42, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::HardwareUUID))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x43, 0xF7]),
-            Ok(OpenDeckMsg::Special(
-                SpecialRequest::FirmwareVersionAndHardwareUUID
-            ))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x4D, 0xF7]),
-            Ok(OpenDeckMsg::Special(
-                SpecialRequest::NrOfSupportedComponents
-            ))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x7F, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::Reboot))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x44, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::FactoryReset))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x55, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::BootloaderMode))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x50, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::NrOfSupportedPresets))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x51, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::BootloaderSupport))
-        );
-        assert_eq!(
-            OpenDeckParser::parse(&[0xF0, 0x00, 0x53, 0x43, 0x00, 0x00, 0x1B, 0xF7]),
-            Ok(OpenDeckMsg::Special(SpecialRequest::Backup))
-        );
-    }
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum SpecialResponse {
+    ValueSize(ValueSize),
+    ValuesPerMessage(u8),
+    FirmwareVersion(u8, u8, u8),
+    HardwareUUID(u32),
+    FirmwareVersionAndHardwareUUID(u8, u8, u8, u32),
+    NrOfSupportedComponents(usize),
+    NrOfSupportedPresets(usize),
+    BootloaderSupport(bool),
+    Backup,
 }
