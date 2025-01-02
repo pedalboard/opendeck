f0f00623bf98f1ce555e3ae512e35badfa11e43f
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-01-02 02:44:14 +0100

    Global Midi Settings

--- src/global/mod.rs
+++ src/global/mod.rs
@@ -34,12 +34,67 @@ impl GlobalPreset {
 
 #[derive(Default)]
 pub struct GlobalMidi {
-    use_global_channel: bool,
-    global_channel: ChannelOrAll,
     standard_note_off: bool,
-    din_midi: bool,
-    ble_midi: bool,
-    usb_to_usb_through: bool,
+    running_status: bool,
+    din_to_usb_thru: bool,
+    din_midi_state: bool,
+    usb_to_din_thru: bool,
+    usb_to_usb_thru: bool,
+    usb_to_ble_thru: bool,
+    din_to_din_thru: bool,
+    din_to_ble_thru: bool,
+    ble_midi_state: bool,
+    ble_to_din_thru: bool,
+    ble_to_usb_thru: bool,
+    ble_to_ble_thru: bool,
+    use_global_midi_channel: bool,
+    global_midi_channel: ChannelOrAll,
+    send_midi_clock: bool,
+}
+
+impl GlobalMidi {
+    pub fn set(&mut self, index: &MidiIndex, value: &u16) {
+        match index {
+            MidiIndex::StandardNoteOff => self.standard_note_off = *value > 0,
+            MidiIndex::RunningStatus => self.running_status = *value > 0,
+            MidiIndex::DINtoUSBthru => self.din_to_usb_thru = *value > 0,
+            MidiIndex::DINMIDIstate => self.din_midi_state = *value > 0,
+            MidiIndex::USBtoDINthru => self.usb_to_din_thru = *value > 0,
+            MidiIndex::USBtoUSBthru => self.usb_to_usb_thru = *value > 0,
+            MidiIndex::USBtoBLEthru => self.usb_to_ble_thru = *value > 0,
+            MidiIndex::DINtoDINthru => self.din_to_din_thru = *value > 0,
+            MidiIndex::DINtoBLEthru => self.din_to_ble_thru = *value > 0,
+            MidiIndex::BLEMIDIstate => self.ble_midi_state = *value > 0,
+            MidiIndex::BLEtoDINthru => self.ble_to_din_thru = *value > 0,
+            MidiIndex::BLEtoUSBthru => self.ble_to_usb_thru = *value > 0,
+            MidiIndex::BLEtoBLEthru => self.ble_to_ble_thru = *value > 0,
+            MidiIndex::UseglobalMIDIchannel => self.use_global_midi_channel = *value > 0,
+            MidiIndex::SendMIDIclock => self.send_midi_clock = *value > 0,
+            MidiIndex::GlobalMIDIchannel => {
+                self.global_midi_channel = ChannelOrAll::from(*value);
+            }
+        }
+    }
+    pub fn get(&mut self, index: &MidiIndex) -> u16 {
+        match index {
+            MidiIndex::StandardNoteOff => self.standard_note_off.into(),
+            MidiIndex::RunningStatus => self.running_status.into(),
+            MidiIndex::DINtoUSBthru => self.din_to_usb_thru.into(),
+            MidiIndex::DINMIDIstate => self.din_midi_state.into(),
+            MidiIndex::USBtoDINthru => self.usb_to_din_thru.into(),
+            MidiIndex::USBtoUSBthru => self.usb_to_usb_thru.into(),
+            MidiIndex::USBtoBLEthru => self.usb_to_ble_thru.into(),
+            MidiIndex::DINtoDINthru => self.din_to_din_thru.into(),
+            MidiIndex::DINtoBLEthru => self.din_to_ble_thru.into(),
+            MidiIndex::BLEMIDIstate => self.ble_midi_state.into(),
+            MidiIndex::BLEtoDINthru => self.ble_to_din_thru.into(),
+            MidiIndex::BLEtoUSBthru => self.ble_to_usb_thru.into(),
+            MidiIndex::BLEtoBLEthru => self.ble_to_ble_thru.into(),
+            MidiIndex::UseglobalMIDIchannel => self.use_global_midi_channel.into(),
+            MidiIndex::SendMIDIclock => self.send_midi_clock.into(),
+            MidiIndex::GlobalMIDIchannel => self.global_midi_channel.into(),
+        }
+    }
 }
 
 #[derive(IntEnum)]
@@ -60,9 +115,31 @@ pub enum PresetIndex {
     EnableMidiChange = 3,
 }
 
+#[derive(Debug, Clone, PartialEq, IntEnum, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u16)]
+pub enum MidiIndex {
+    StandardNoteOff = 0x0,
+    RunningStatus = 0x1,
+    DINtoUSBthru = 0x2,
+    DINMIDIstate = 0x3,
+    USBtoDINthru = 0x4,
+    USBtoUSBthru = 0x5,
+    USBtoBLEthru = 0x6,
+    DINtoDINthru = 0x7,
+    DINtoBLEthru = 0x8,
+    BLEMIDIstate = 0x9,
+    BLEtoDINthru = 0xA,
+    BLEtoUSBthru = 0xB,
+    BLEtoBLEthru = 0xC,
+    UseglobalMIDIchannel = 0xD,
+    GlobalMIDIchannel = 0xE,
+    SendMIDIclock = 0xF,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum GlobalSection {
-    Midi(u16, u16),
+    Midi(MidiIndex, u16),
     Presets(PresetIndex, u16),
 }
