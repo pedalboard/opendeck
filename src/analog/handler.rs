bcb9b57fe2bccd514ef52ea9c4be2d36ba99491c
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-02-25 18:44:24 +0100

    Try iterator approach

--- src/analog/handler.rs
+++ src/analog/handler.rs
@@ -2,30 +2,37 @@ use crate::analog::{Analog, AnalogMessageType};
 
 use midi2::{channel_voice1::ControlChange, error::BufferOverflow, prelude::*, BytesMessage};
 
-//struct AnalogIterator
+pub struct AnalogMessages<'a> {
+    analog: &'a mut Analog,
+    value: u16,
+}
+impl<'a> AnalogMessages<'a> {
+    pub fn new(analog: &'a mut Analog, value: u16) -> Self {
+        Self { analog, value }
+    }
+    pub fn next<'buf>(&mut self, buffer: &'buf mut [u8]) -> Option<BytesMessage<&'buf mut [u8]>> {
+        match self.analog.message_type {
+            AnalogMessageType::Button => {
+                let mut m = ControlChange::try_new_with_buffer(buffer).unwrap();
+                m.set_channel(self.analog.channel.into_midi());
+                m.set_control(u7::new(self.analog.midi_id as u8));
+                m.set_control_data(u7::new(self.value as u8));
+                Some(m.into())
+            }
+            AnalogMessageType::PotentiometerWithCCMessage7Bit => None,
+            AnalogMessageType::PotentiometerWithNoteMessage => None,
+            AnalogMessageType::FSR => None,
+            AnalogMessageType::NRPN7 => None,
+            AnalogMessageType::NRPN8 => None,
+            AnalogMessageType::PitchBend => None,
+            AnalogMessageType::PotentiometerWithCCMessage14Bit => None,
+        }
+    }
+}
 
 impl Analog {
-    pub fn handle<'a>(
-        &mut self,
-        value: u16,
-        buffer: &'a mut [u8],
-    ) -> Result<Option<BytesMessage<&'a mut [u8]>>, BufferOverflow> {
-        match self.message_type {
-            AnalogMessageType::Button => {
-                let mut m = ControlChange::try_new_with_buffer(buffer)?;
-                m.set_channel(self.channel.into_midi());
-                m.set_control(u7::new(self.midi_id as u8));
-                m.set_control_data(u7::new(value as u8));
-                Ok(Some(m.into()))
-            }
-            AnalogMessageType::PotentiometerWithCCMessage7Bit => Ok(None),
-            AnalogMessageType::PotentiometerWithNoteMessage => Ok(None),
-            AnalogMessageType::FSR => Ok(None),
-            AnalogMessageType::NRPN7 => Ok(None),
-            AnalogMessageType::NRPN8 => Ok(None),
-            AnalogMessageType::PitchBend => Ok(None),
-            AnalogMessageType::PotentiometerWithCCMessage14Bit => Ok(None),
-        }
+    pub fn handle<'a>(&'a mut self, value: u16) -> AnalogMessages<'a> {
+        AnalogMessages::new(self, value)
     }
 }
 
@@ -45,13 +52,13 @@ mod tests {
             lower_limit: 127,
             lower_adc_offset: 0,
             upper_adc_offset: 0,
-            message_type: AnalogMessageType::PotentiometerWithCCMessage7Bit,
+            message_type: AnalogMessageType::Button,
             midi_id: 0x03,
             channel: ChannelOrAll::default(),
         };
-        let result = button.handle(1000, &mut message_buffer).unwrap().unwrap();
+        let mut result = button.handle(10);
 
-        let buf = result.data();
-        assert_eq!(buf, [0x90, 0x03, 0x7F]);
+        let m = result.next(&mut message_buffer).unwrap();
+        assert_eq!(m.data(), [176, 0x03, 10]);
     }
 }
