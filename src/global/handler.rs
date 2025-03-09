5dd349e4fd09de87aee5717ba61a35879d5451a2
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-03-09 16:07:39 +0100

    Add enum next method

--- src/global/handler.rs
+++ src/global/handler.rs
@@ -1,8 +1,23 @@
 use crate::analog::handler::AnalogMessages;
 use crate::button::handler::ButtonMessages;
 
+use midi2::{error::BufferOverflow, BytesMessage};
+
 pub enum Messages<'a> {
     Button(ButtonMessages<'a>),
     Analog(AnalogMessages<'a>),
     None,
+}
+
+impl Messages<'_> {
+    pub fn next<'buf>(
+        &mut self,
+        buffer: &'buf mut [u8],
+    ) -> Result<Option<BytesMessage<&'buf mut [u8]>>, BufferOverflow> {
+        match self {
+            Messages::Button(m) => m.next(buffer),
+            Messages::Analog(m) => m.next(buffer),
+            Messages::None => Ok(None),
+        }
+    }
 }
